ff1fbb424133eac1ecf2e3448026fccbe3618e77
Rqbln <118724817+Rqbln@users.noreply.github.com>
2022-12-05 12:32:58 +0100

    Update sprog.h

---
+++ sprog.h
@@ -0,0 +1,13 @@
+//
+// Created by bapti on 30/11/2022.
+//
+
+#ifndef LABYRINTE_SPROG_H
+#define LABYRINTE_SPROG_H
+
+#endif //LABYRINTE_SPROG_H
+
+void initialisationTableau(int tab[7][7]);
+void affichageTableau(int tab[7][7]);
+void essaieJoueur(int *ligne, int *colonne, int *point, int tab[10][10]);
+void finJeu(int *ligne, int *colonne, int tab[7][7], int*n);
