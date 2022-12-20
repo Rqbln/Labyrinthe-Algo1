dd26e5a6ef48120dfcd76e7410a39448b139fb1f
baptiste321 <baptiste.chesnot@gmail.com>
2022-12-20 21:38:46 +0100

    baptiste3.0

--- initPlateauColor.c
+++ initPlateauColor.c
@@ -19,7 +19,7 @@ void bg_color(int color){
 }
 //change la couleur du texte et du fond
 
-void afficherTerrain4(int tab[LARGEUR][LARGEUR], int taille){
+/*void afficherTerrain4(int tab[LARGEUR][LARGEUR], int taille){
     int caseT1[3][3]={0,0,0,
                       1,1,1,
                       0,1,0,};
@@ -60,7 +60,7 @@ void afficherTerrain4(int tab[LARGEUR][LARGEUR], int taille){
                       1,1,1,
                       0,0,0};
 
-    int i,j;
+    int i;
     printf("\n\n    |");
     bg_color(CYAN);
     for(i=0;i<taille;i++){
@@ -78,7 +78,7 @@ void afficherTerrain4(int tab[LARGEUR][LARGEUR], int taille){
         bg_color(BLACK);
         printf("|");
         bg_color(WHITE);
-        for(j=0;j<taille;j++){
+        for(int j=0;j<taille;j++){
             if(tab[i][j] == 1,2,6,50){ //T1
                 for (int k = 0; k < (3); k++) {
                     for (int l = 0; l < (3); j++) {
@@ -231,4 +231,202 @@ void afficherTerrain4(int tab[LARGEUR][LARGEUR], int taille){
         bg_color(BLACK);
         printf("\n");
     }
+}
+*/
+void afficherTerrain4(int tab[LARGEUR][LARGEUR], int taille) {
+    // Declare arrays for the different shapes
+    int caseT1[3][3] = {0,0,0, 1,1,1, 0,1,0};
+    int caseT2[3][3] = {0,1,0, 1,1,0, 0,1,0};
+    int caseT3[3][3] = {0,1,0, 1,1,1, 0,0,0};
+    int caseT4[3][3] = {0,1,0, 0,1,1, 0,1,0};
+    int caseL1[3][3] = {0,1,0, 0,1,1, 0,0,0};
+    int caseL2[3][3] = {0,0,0, 0,1,1, 0,1,0};
+    int caseL3[3][3] = {0,0,0, 1,1,0, 0,1,0};
+    int caseL4[3][3] = {0,1,0, 1,1,0, 0,0,0};
+    int caseI1[3][3] = {0,1,0, 0,1,0, 0,1,0};
+    int caseI2[3][3] = {0,0,0, 1,1,1, 0,0,0};
+
+    // Set the background color for the grid
+    bg_color(BLACK);
+
+    // Print the grid
+    printf("\n\n    |");
+    bg_color(CYAN);
+    for(int i=0; i<taille; i++) {
+        printf(" %2c  ", i+'A');
+    }
+    bg_color(BLACK);
+    printf("\n-----");
+    for(int i=0; i<taille; i++) {
+        printf("-----");
+    }
+    printf("\n");
+    for(int i=0; i<taille; i++) {
+        bg_color(CYAN);
+        printf(" %2d ", i);
+        bg_color(BLACK);
+        printf("|");
+        for(int j=0; j<taille; j++) {
+            int value = tab[i][j];
+            if(value == 1 || value == 2 || value == 6 || value == 50) {
+                // T1 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseT1[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseT1[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 7 || value == 10 || value == 11 || value == 51) {
+                // T2 shape
+                for (int k = 0; k < 3;k++) {
+
+                        for (int l = 0; l < 3; l++) {
+                            if (caseT3[k][l] == 0) {
+                                bg_color(ORANGE);
+                                printf(" %2c  ", ' ');
+                            }
+                            if (caseT3[k][l] == 1) {
+                                bg_color(YELLOW);
+                                printf(" %2c  ", ' ');
+                            }
+                        }
+                    }
+
+            }
+            else if(value == 9 || value == 13 || value == 14 || value == 52) {
+                // T3 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseT3[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseT3[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 4 || value == 5 || value == 8 || value == 53) {
+                // T3 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseT4[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseT4[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 12 || value == 60 || value == 70) {
+                // L1 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseL1[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseL1[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 0 || value == 61 || value == 71) {
+                // L2 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseL2[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseL2[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 3 || value == 62 || value == 72) {
+                // L3 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseL3[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseL3[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 15 || value == 63 || value == 73) {
+                // L4 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseL4[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseL4[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 80) {
+                // I1 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseI1[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseI1[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+            }
+            else if(value == 81){
+                // I2 shape
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        if (caseI2[k][l] == 0) {
+                            bg_color(ORANGE);
+                            printf(" %2c  ", ' ');
+                        }
+                        if (caseI2[k][l] == 1) {
+                            bg_color(YELLOW);
+                            printf(" %2c  ", ' ');
+                        }
+                    }
+                }
+        }
+        else {
+            // Empty space
+            bg_color(BLACK);
+            printf(" %2c  ", ' ');
+        }
+        bg_color(BLACK);
+    }
+    printf("\n");
+}
 }
